2075b27ca7539478c13da3f89fee9d051b05f471
RicardoCordovilla <ricardocordovillaf@gmail.com>
2022-10-28 14:37:20 -0500

    cambios textos

--- sheets.js
+++ sheets.js
@@ -21,7 +21,7 @@ form.addEventListener('submit', (e) => {
 
     setTimeout(() => {
         form.btn_enviar.innerHTML = 'Datos guardados correctamente'
-    }, 1000)
+    }, 500)
 
 
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
